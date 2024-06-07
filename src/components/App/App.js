669c294007b5c5fa98e2f13f33384802b14efcfe
Josue Flores <josue.flores20@gmail.com>
2024-06-07 08:07:46 -0500

    add route functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
-import "./App.css";
+import React from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Profile from "../Profile/Profile";
@@ -6,12 +7,22 @@ import Footer from "../Footer/Footer";
 import RegisterModal from "../RegisterModal/RegisterModal";
 import LoginModal from "../LoginModal/LoginModal";
 
+import "./App.css";
+
 function App() {
+  const [isLoggedIn, setLoggedIn] = React.useState(false);
+
   return (
     <div className="App">
       <Header />
-      {/* <Main /> */}
-      <Profile />
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
       <Footer />
       {/* <LoginModal /> */}
       {/* <RegisterModal /> */}

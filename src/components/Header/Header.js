73f6e68042885481011aa77b65e4de8e111ca868
Josue Flores <josue.flores20@gmail.com>
2024-06-08 13:29:31 -0500

    added navlink functionality to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import "./Header.css";
 
 function Header() {
@@ -8,12 +8,21 @@ function Header() {
       <div className="header__elements">
         <h1 className="header__title"> Sports News</h1>
         <div className="header__nav">
-          <Link to="/" className="header__button header__button-home">
+          <NavLink
+            exact
+            to="/"
+            activeClassName="header__button-home_active"
+            className="header__button header__button-home"
+          >
             Home
-          </Link>
-          <Link to="/profile" className="header__button header__button-profile">
+          </NavLink>
+          <NavLink
+            to="/profile"
+            activeClassName="header__button-profile_active"
+            className="header__button header__button-profile"
+          >
             Profile
-          </Link>
+          </NavLink>
           <button className="header__button header__button-signup" type="click">
             Sign up
           </button>

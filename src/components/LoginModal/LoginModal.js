4453eb09f0d7d5af620242573a69715dccebf62e
Josue Flores <josue.flores20@gmail.com>
2024-06-12 13:43:10 -0500

    complete styling for signup/signin modals and make responsive

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -35,36 +35,37 @@ function LoginModal({
       buttonText="Log In"
     >
       <div className="login__modal">
-        <div className="login__modal-inputs"></div>
-        <label className="login__modal_input-label">
-          <div className="login__modal_input-title">Email</div>
-          <input
-            className="login__modal_input"
-            type="email"
-            name="email"
-            placeholder="Email"
-            // value={email}
-            // onChange={handleEmailChange}
-          />
-        </label>
-        <label className="login__modal_input-label">
-          <div className="login__modal_input-title">Password</div>
-          <input
-            className="login__modal_input"
-            type="text"
-            name="password"
-            placeholder="Password"
-            // value={password}
-            // onChange={handlePasswordChange}
-          />
-        </label>
-        <button
-          className="login__modal_signup"
-          type="button"
-          onClick={handleSignUpModal}
-        >
-          or Sign Up
-        </button>
+        <div className="login__modal-inputs">
+          <label className="login__modal_input-title">
+            <div>Email</div>
+            <input
+              className="login__modal_input"
+              type="email"
+              name="email"
+              placeholder="Email"
+              // value={email}
+              // onChange={handleEmailChange}
+            />
+          </label>
+          <label className="login__modal_input-title">
+            <div>Password</div>
+            <input
+              className="login__modal_input"
+              type="text"
+              name="password"
+              placeholder="Password"
+              // value={password}
+              // onChange={handlePasswordChange}
+            />
+          </label>
+          <button
+            className="login__modal_signup"
+            type="button"
+            onClick={handleSignUpModal}
+          >
+            or Sign Up
+          </button>
+        </div>
       </div>
     </ModalWithForm>
   );
